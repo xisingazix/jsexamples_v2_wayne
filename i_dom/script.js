c15f5d82d72e0eee224a94235b5300a4f300a89e
xisingazix <xisingazix@hotmail.com>
2025-12-05 16:04:25 +0800

    dom manipulation lesson 1

--- i_dom/script.js
+++ i_dom/script.js
@@ -7,19 +7,21 @@ const pagetitle = document.getElementById("pageTitle");
 // Setting HTML markup contained within the element.
 
 // Implement Code here
+pagetitle.innerHTML = "<pre>Document Object Model: DOM<pre>";
 
 
 // 2. element.innerText
 // Rendering text content of a node and its descendants.
 
 // Implement Code here
-
+pagetitle.innerText = "document object model".toUpperCase();
 
 // 3. element.style
 // Modifying the style of HTML elements
 
 // Implement Code here
-
+pagetitle.style.height = "2 rem";
+pagetitle.style.color = "#f00";
 
 // 4. getElementById
 // refer to the example above (line 4)
@@ -29,33 +31,130 @@ const pagetitle = document.getElementById("pageTitle");
 // Using getElementsByTagName method, search for elements that match a tag name
 
 // Implement Code here
+const spans = document.getElementsByTagName("span");
+//returns an html collection object,
+const spanInputs =[
+    "New text 1",
+    "New text 2",
+    "New text 3",
+    "Span No. 4",
+    'Span No. 5'
+]
+
+
+
+spans[0].innerHTML = "<p>properties of the DOM</p>";     // inner HTML able to change the html tag
+spans[1].innerText = "methods of the DOM \n";            // \n can insert start new line with java script
+spans[2].style.display = "block";       // using style to start new line for test 3
 
 // Using getElementsByTagName method, search for elements that match a tag's index
 
 // Implement Code here
+for (let i = 0; i < spans.length; i++) {
+    spans[i].style.display = "block";
+    spans[i].style.fontWeight = "bolder";
+    spans[i].innerText = spanInputs[i];
+}
 
-
-// 6. createElement
+// 6. createElement 
 // Adding a new HTML element in JavaScript.
 
 // Implement Code here
+const paragraph = document.createElement("p");  //will recognise as <p>  ,create html tag elements
+paragraph.innerText = "Appended Paragraph";
+document.body.append(paragraph);        
 
 
 // 7. querySelector
 // Selecting the fist instance of h2 tag and adding an innerText
+// *        # - querySelector recognises a id
+// *        . - querySelector recognises a  class
+// *        input[type = "type_name"] - requerySelector recognises the inputs(s)
+
+const heading2 = document.querySelectorAll("h2");       //targeting method operates like the css
+heading2.innerText = "Heading 2 - All selector";
+
+const header2 = document.querySelector("h2");    //querySelector only target the first instance   
+header2.innerText = "Heading 2 - tag selector";     
+
+const header3 = document.querySelector(".heading3");
+header3.innerText = "Heading 3 - class selector";
+
+const header4 = document.querySelector("#heading4");
+header4.innerText = "Heading 4 - id selector";
+
+const inputTxt = document.querySelector('input[type ="text"]');
+inputTxt.value = "Input selector engaged";
+
+
+
 
 // Implement Code here
 
 // Using querySelectorAll method, search for elements that match a tag's class
 
 // Implement Code here
+const updateListItems = document.querySelectorAll(".menuItem");
 
+//updateListItems[0].innerText = "menu item 1";
+//updateListItems[1].innerText = "menu item 2";
+//updateListItems[2].innerText = "menu item 3";
+
+
+//create a horizontal menu
+// each menu item to be a link with specific styles
+const ulMenuBar = document.querySelector("ul");
+ulMenuBar.style.display = "flex";
+ulMenuBar.classList.add("d-flex", "py-4");
+ulMenuBar.setAttribute("name", "menubar");
+
+// TODO: set the bar to 100% according to screen width
+
+const menuTitles = ["Welcome", "About", "Product", "Contact"];
+
+menuTitles.forEach((title, index) => {
+
+    //create a list item to append (as a child) to the unordered list
+    const listItem = document.createElement("li");
+    listItem.setAttribute("name", `nav-menu-item`);
+    listItem.setAttribute("key", `nav-menu-item-${index +1}`);
+    listItem.classList.add("mx-auto", "list-unstyled");
+    ulMenuBar.appendChild(listItem);
+
+    //create anchor tag to insert into each list items
+    const anchor = document.createElement("a");
+    anchor.innerText = title;
+    anchor.setAttribute("href", "#");
+    anchor.setAttribute("name", `navlink`);
+    anchor.classList.add("btn", "btn-success");
+
+    listItem.appendChild(anchor);
+})
+
+
+//Learn to target the name of an element
+const navbarElement = document.getElementsByName("menubar");
+navbarElement[0].classList.add("bg-dark");
+
+//Learn to  target the elements with the same name
+const navMenuElements = document.getElementsByName("nav-menu-item");
+for (let index = 0; index < navMenuElements.length; index++) {
+    const element = navMenuElements[index];
+    element.classList.add("border", "rounded", "border-2");
+}
 
 // 8. element.append()
 // Adding a new HTML element inside an existing element.
 
 // Implement Code here
+const headerTwoGreeting = document.createElement("h2");
+headerTwoGreeting.innerText = "Welcome to";
 
+const amendTitle = document.body.appendChild(headerTwoGreeting);
+console.log("Target", amendTitle);
+
+
+amendTitle.innerText = "Welcome to World of JavaScript";
 
 // 9. parentNode.appendChild(node)
 // Adding a node using appendChild
